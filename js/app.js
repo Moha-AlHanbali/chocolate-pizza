a3bf66cc3f27caa2ea5bd62df9d2a770fd800629
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-07-02 16:25:13 +0300

    added JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
 'use strict';
+
+
+let idArray = ['milk', 'mascarpone', 'salt', 'figs', 'brownSugar', 'water', 'cream', 'sugar', 'eggs', 'lemon', 'butter', 'pecans'];
+let labelArray = ['milkLabel', 'mascarponeLabel', 'saltLabel', 'figsLabel', 'brownSugarLabel', 'waterLabel', 'creamLabel', 'sugarLabel', 'eggsLabel', 'lemonLabel', 'butterLabel', 'pecansLabel'];
+let newArray = [];
+
+function Strike(){
+    this.elementID = '';
+    this.elementLabel = '';
+
+    newArray.push(this);
+}
+
+Strike.prototype.addElement= function(arrayId, arrayLabel){
+    this.elementID = document.getElementById(arrayId);
+    this.elementLabel = document.getElementById(arrayLabel);
+}
+
+
+Strike.prototype.check= function(){
+    if (this.elementID.checked === true) {
+        this.elementLabel.style.textDecoration = 'line-through';
+    } else{
+        this.elementLabel.style.textDecoration = 'none'
+    }
+}
+
+
+
+let checkStrike = document.getElementById('checkStrike');
+
+checkStrike.addEventListener('change', strikeThrough);
+
+function strikeThrough(event){
+    event.preventDefault();
+
+    newArray = [];
+    for (let i=0; i<idArray.length; i++){
+        let genID = new Strike();
+        genID.addElement(idArray[i], labelArray[i]);
+        genID.check();
+    }
+
+}
